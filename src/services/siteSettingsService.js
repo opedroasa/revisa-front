c093ea8af37da8a9324d1a3ae4e5d6c680fdd3a4
Pedro Alexandre <pedroantonio2502@gmail.com>
2025-10-15 17:45:54 -0300

    Commit 15/10/2025

--- src/services/siteSettingsService.js
+++ src/services/siteSettingsService.js
@@ -7,7 +7,7 @@ export const SiteSettingsService = {
   // admin (precisa estar logado)
   getAdmin: () => api.get("/api/site/settings"),
 
-  // atualizar configurações (precisa auth)
+  // atualizar configurações (precisa auth).
   update: ({ whatsappPhone }) =>
     api.put("/api/site/settings", { whatsappPhone }),
 };
